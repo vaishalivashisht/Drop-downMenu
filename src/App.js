4c6cd7ca7807a98ffc513ff4188720330e3e9438
vaishali sharma <vaishalivashisht@gmail.com>
2024-03-07 00:22:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
-import React, { useState } from 'react';
-import './dropdown.css';
+import React, { useState } from 'react'; 
+import './dropdown.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+
+function App() { 
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [value, setValue] = useState(''); 
   
@@ -10,11 +13,11 @@ function App() {
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
-  };
+  }; 
 
-  const handleClick = (e) => {
+  const handleClick = (e) => { 
     setValue(e.target.textContent);
-    setDropdownOpen(false); // Optionally close the dropdown after a selection
+    setDropdownOpen(false); // Optionally close the dropdown after a selection 
   };
 
   const closeDropdown = (event) => {
@@ -25,21 +28,22 @@ function App() {
 
   return ( 
     <div className="App" onClick={closeDropdown}>
-      <h2> THE DROPDOWN </h2>
+      <h2> THE DROPDOWN </h2> 
       <h3>{value}</h3> {/* Update this line to show the selected value */}
   
       <div className="dropdown">
-        <button onMouseOver={toggleDropdown} className="dropbtn">Dropdown </button>
+        <button onMouseOver={toggleDropdown} className="dropbtn"> Dropdown </button>
+        <span className='icon-arrow'><FontAwesomeIcon icon={faChevronDown} /></span>
         <div id="myDropdown" className={dropdownOpen ? "dropdown-content show" : "dropdown-content"}>
           {list.map((item, index) => {
             // Add the onClick handler to each dropdown item
             return (
               <a href="#!" key={index} onClick={handleClick}>
-                {item}
+                {item} 
               </a>
             );
           })}
-        </div>
+        </div> 
       </div>
     </div>
   );
