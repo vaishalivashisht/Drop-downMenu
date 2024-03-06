c95df9bb2b773fda6b63cd25dd5d5d5fae9d1a0f
vaishali sharma <vaishalivashisht@gmail.com>
2024-03-06 13:43:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 
+import React, { useState } from 'react';
 import './dropdown.css';
-import Dropdown from './Dropdown';
-import { useState } from 'react';
-
 
 function App() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [value, setValue] = useState(''); 
+  
+  const list = ['React', 'Js', 'Java', 'Html'];
 
-  const [selected, setSelected] = useState("");
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
-  return (
-    <div className="App">
-      
-    <h1> The Drop Down Menu </h1>
+  const handleClick = (e) => {
+    setValue(e.target.textContent);
+    setDropdownOpen(false); // Optionally close the dropdown after a selection
+  };
 
-    <Dropdown selected={selected} setSelected={setSelected}/>
+  const closeDropdown = (event) => {
+    if (!event.target.matches('.dropbtn')) {
+      setDropdownOpen(false);  
+    }  
+  };  
 
+  return ( 
+    <div className="App" onClick={closeDropdown}>
+      <h2> THE DROPDOWN </h2>
+      <h3>{value}</h3> {/* Update this line to show the selected value */}
+  
+      <div className="dropdown">
+        <button onMouseOver={toggleDropdown} className="dropbtn">Dropdown </button>
+        <div id="myDropdown" className={dropdownOpen ? "dropdown-content show" : "dropdown-content"}>
+          {list.map((item, index) => {
+            // Add the onClick handler to each dropdown item
+            return (
+              <a href="#!" key={index} onClick={handleClick}>
+                {item}
+              </a>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
-}
+} 
 
 export default App;
-
-
